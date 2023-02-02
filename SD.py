2ff43d5186c0b3f07268c4f4ba9366ab55b69551
FatemehMomeni <50900356+FatemehMomeni@users.noreply.github.com>
2023-02-02 19:55:36 +0330

    Small Changes...

--- SD.py
+++ SD.py
@@ -4,13 +4,13 @@ import pandas as pd
 import advertools as adv
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
-import tensorflow as tf
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 
 # Set the display width to see more of the contents of data
-pd.set_option('max_colwidth', 800)
+pd.set_option('display.width', 300)
+pd.set_option('display.max_columns', 10)
 
 # Get stop words
 stop_words = list(adv.stopwords['english'])
@@ -18,6 +18,8 @@ stop_words = list(adv.stopwords['english'])
 # Load datasets
 train = pd.read_csv('data/traindata.txt', sep='\t', encoding='windows-1252')
 test = pd.read_csv('data/testdata.txt', sep='\t', encoding='windows-1252')
+
+print('\t\tOrigin train dataset:\n\n', train.head())
 
 # Remove unnecessary columns
 train.drop(axis=1, columns=['Opinion towards', 'Sentiment'], inplace=True)
@@ -41,12 +43,14 @@ test['Tweet'] = test['Tweet'].apply(lambda x: ' '.join([word for word in x.split
 train['Stance'].replace(['FAVOR', 'AGAINST', 'NONE'], [1, 2, 0], inplace=True)
 test['Stance'].replace(['FAVOR', 'AGAINST', 'NONE'], [1, 2, 0], inplace=True)
 
+print('\n\t\tEdited train dataset:\n\n', train.head())
+
 # call tokenizer
 # train
-train_tokenizer = Tokenizer(num_words=2815, filters='None', lower=False, split=' ')
+train_tokenizer = Tokenizer(num_words=2815, filters='None', lower=True)
 train_tokenizer.fit_on_texts(train)
 # test
-test_tokenizer = Tokenizer(num_words=1250, filters='None', lower=False, split=' ')
+test_tokenizer = Tokenizer(num_words=1250, filters='None', lower=True)
 test_tokenizer.fit_on_texts(test)
 
 # Separate data and labels
@@ -54,12 +58,10 @@ test_tokenizer.fit_on_texts(test)
 x_train = np.array(train_tokenizer.texts_to_sequences(train['Tweet'].values))
 x_train = pad_sequences(x_train, padding='post', maxlen=100)
 y_train = np.asarray(train['Stance'].values)
-train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train))
 # test
 x_test = np.array(test_tokenizer.texts_to_sequences(test['Tweet'].values))
 x_test = pad_sequences(x_test, padding='post', maxlen=100)
 y_test = np.asarray(test['Stance'].values)
-test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test))
 
 # Train the random forest model
 rf = RandomForestClassifier()
